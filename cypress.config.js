1ba7224f6ac5035d2acae4fa9fe3ccae3ef532de
jimmycalhado <jimmyandrey.1208@gmail.com>
2025-03-25 22:57:25 -0300

    teste

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: "cypress/integration/**/*.spec.js",
+    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+      on("before:run", () => {
+        console.log("Iniciando os testes E2E...");
+      });
+      return config;
+    },
+  },
+
+  component: {
+    specPattern: "cypress/component/**/*.cy.js",
+    supportFile: "cypress/support/component.js",
+    setupNodeEvents(on, config) {
+      on("before:run", () => {
+        console.log("Iniciando os testes Unitários...");
+      });
+      return config;
+    },
+  },
+});
